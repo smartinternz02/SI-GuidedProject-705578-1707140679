14aeedf2ed1f13b74def46a4bdefb1f67542e0c9
ShaikPeerFarahath <139475379+ShaikPeerFarahath@users.noreply.github.com>
2024-02-11 17:38:06 +0530

    Uploaded Test Suites

---
+++ Test Suites/CURA_TestSuite/TL_CURA_AppointmentConfirmation_TestSuite_001.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TL_CURA_AppointmentConfirmation_TestSuite_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b8f9632e-933c-4911-9f63-758d9df58d11</testSuiteGuid>
+   <testCaseLink>
+      <guid>1de89dfa-a65f-460d-8399-53fd6f3c7f04</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestListener1/TC_TL_AppointmentConfirmation_001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
